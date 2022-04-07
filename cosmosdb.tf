695521376b33027bc4844b1eec57ed92ade92955
Amresh Krishnamurthy <amresh.krishnamurthy@maersk.com>
2022-04-07 15:16:28 +0530

    TF scripts
    Scripts added to create cosmosdb mongo api

---
+++ cosmosdb.tf
@@ -0,0 +1,53 @@
+# resource "azurerm_resource_group" "rg" {
+#   name     = "rg-terraform-westeu-002"
+#   location = "westeurope"
+# }
+
+resource "azurerm_cosmosdb_account" "cosmosdbaccount" {
+  name                = "cosmos-mongo-001"
+  location            = "westeurope"
+  resource_group_name = "ohp-documentation-dev"
+  offer_type          = "Standard"
+  geo_location {
+    location          = "westeurope"
+    failover_priority = 0
+  }
+  kind                = "MongoDB"
+  capabilities {
+    name = "EnableMongo"
+    //EnableCassandra, EnableGremlin, EnableMongo, EnableTable, 
+  }
+  consistency_policy {
+    consistency_level       = "Session"  
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "fooddatabase" {
+  name                = "products-db"
+  resource_group_name = azurerm_cosmosdb_account.cosmosdbaccount.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdbaccount.name
+#   throughput          = 400
+#   autoscale_settings {
+#       max_throughput = 4000
+#   }
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "nutritioncollection" {
+  name                = "nutrition-coll"
+  resource_group_name = azurerm_cosmosdb_account.cosmosdbaccount.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdbaccount.name
+  database_name       = azurerm_cosmosdb_mongo_database.fooddatabase.name
+
+  #default_ttl_seconds = "4500"
+  shard_key           = "foodCategory" 
+  autoscale_settings {
+      max_throughput = 4000
+  }
+  index {
+    keys   = ["_id"]
+    unique = true
+  }
+  index {
+    keys   = ["productKey"]   
+  }
+}
---
+++ main.tf
@@ -0,0 +1,19 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.85"
+    }
+  }
+  required_version = ">= 0.15.3"
+}
+
+provider "azurerm" {
+  features {
+      
+  }
+  skip_provider_registration = true
+}
+
+
